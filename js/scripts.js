c606dc66eed8bc4c099cc57bb803f6c1b75c72fd
james nielson & Kyle Bulloch <student@epicodus.com>
2015-04-13 12:15:59 -0700

    Add form

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  $("form#skylark").submit(function(event) {
+    var task_tisk = $("input#task").val();
+    var priori = $("input#prioritaah").val(); 
+
+
+    event.preventDefault();
+  });
+
+});
