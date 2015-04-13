4e9706562472b1d192f59925f214974b6fea8378
james nielson & Kyle Bulloch <student@epicodus.com>
2015-04-13 12:18:48 -0700

    Add list to show nomes du pluyme

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,11 @@ $(document).ready(function() {
 
   $("form#skylark").submit(function(event) {
     var task_tisk = $("input#task").val();
-    var priori = $("input#prioritaah").val(); 
+    var priori = $("input#prioritaah").val();
+
+    var task = { nome: task_tisk, priority: priori };
+
+    $("#banana_patch").append("<li><span class='task'>" + task.nome + "</span></li>");
 
 
     event.preventDefault();
