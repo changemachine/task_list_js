78a0e458dd27adcd9db4ebd3ade3fe36267d0cbc
james nielson & Kyle Bulloch <student@epicodus.com>
2015-04-13 16:28:39 -0700

    Ability to remove/un-remove tasks.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,33 @@
 $(document).ready(function() {
   $("#task").focus();
+  var id = 0;
   $("form#skylark").submit(function(event) {
     event.preventDefault();
     var task_tisk = $("input#task").val();
     var priori = $("select#prioritaah").val();
-    var task = { nome: task_tisk, priority: priori };
+    var task = {
+      nome: task_tisk,
+      priority: priori,
+      ident: id++,
+    };
 
-    $("#banana_patch").append("<li><span class='task'>" + task.nome + "</span></li>");
+    $("#banana_patch").append("<li><span class='task' id=" + task.ident + ">" + task.nome + "</span>");
 
     $(".task").last().click(function() {
-      $("#show-tusk").toggle();
+      $("#show-tusk").show();
       $(".tusk").text(task.nome);
       $(".priority").text(task.priority);
+      $(".id_span").text(task.ident);
+    });
+
+    $(".task").last().dblclick(function() {
+      $("span#" + task.ident).toggleClass("done");
     });
 
     $("#banana_patch").show();
     this.reset();
   });
+
+
 
 });
