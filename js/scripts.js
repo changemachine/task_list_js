b5258e8bc88030a42875a2c7afded357a69d7dc2
james nielson & Kyle Bulloch <student@epicodus.com>
2015-04-13 12:27:52 -0700

    Add display for details

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,19 @@ $(document).ready(function() {
 
   $("form#skylark").submit(function(event) {
     var task_tisk = $("input#task").val();
-    var priori = $("input#prioritaah").val();
+    var priori = $("select#prioritaah").val();
 
     var task = { nome: task_tisk, priority: priori };
 
     $("#banana_patch").append("<li><span class='task'>" + task.nome + "</span></li>");
 
+    $(".task").click(function() {
+      $("#show-tusk").toggle();
+      $(".task").text(task.nome);
+      $(".priority").text(task.priority);
+    })
 
+    $("#banana_patch").show();
     event.preventDefault();
   });
 
